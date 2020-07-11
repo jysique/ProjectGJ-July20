aa81971eae8bc86f697bc17667e1cb779a069913
jysique <joseysique@gmail.com>
2020-07-11 11:15:14 -0500

    player

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -18,21 +18,27 @@ public class GameController : MonoBehaviour
     private Transform panelPlayers;
     private void Awake() {
         players  = new Player[current_players];
+        InitPlayers();
         array = new Button[current_players];
-    }
+        InstantiatePlayers();
+        ButtonsFuncionality();
 
+    }
+    int current_index = 0;
     void Start()
     {
         panelStats.SetActive(false);
-        InstantiatePlayers();
+    }
+    void ButtonsFuncionality(){
         for(int i = 0; i < array.Length; i++){
             int index = i;
+            current_index = i;
+            array[i].transform.GetChild(0).GetComponent<Text>().text = players[i].name;
             array[i].onClick.AddListener(() => ShowPlayerStats(index));
         }
     }
     void Update()
     {
-        
     }
 
     void ShowPlayerStats(int index){
@@ -41,13 +47,18 @@ public class GameController : MonoBehaviour
         panelStats.transform.GetChild(2).GetComponent<Slider>().value = players[index].hunger;
         panelStats.transform.GetChild(3).GetComponent<Slider>().value = players[index].salubrity;
         panelStats.transform.GetChild(4).GetComponent<Slider>().value = players[index].fatigue;
-
-        panelStats.transform.GetChild(5).GetComponent<Text>().text="SEX: "+ players[index].sex.ToString();
+        string _sex = (players[index].sex)? "HOMBRE":" MUJER";
+        panelStats.transform.GetChild(5).GetComponent<Text>().text="SEX: "+ _sex;
         panelStats.transform.GetChild(6).GetComponent<Text>().text="STATUS: "+ players[index].status;
         panelStats.transform.GetChild(7).GetComponent<Text>().text="CAPACITY:"+ players[index].capacity.ToString();
         panelStats.transform.GetChild(8).GetComponent<Text>().text="WEIGHT MAX:"+ players[index].max_weight.ToString();
+        StartCoroutine(ClosePanelStats());
     }
 
+    IEnumerator ClosePanelStats(){
+        yield return new WaitForSeconds(5);
+        panelStats.SetActive(false);
+    }
     void InstantiatePlayers(){
         if (players.Length<= max_players)
         {
@@ -55,8 +66,21 @@ public class GameController : MonoBehaviour
             {
                 GameObject a = Instantiate(prefabPlayer);
                 a.transform.SetParent(panelPlayers.transform,false);
-                array[i] = a.GetComponent<Button>();   
+                array[i] = a.GetComponent<Button>();
             }
         }
+    }
+    void InitPlayers(){
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i] = new Player();   
+            players[i].sanity = 100;
+            players[i].salubrity = 100;
+            players[i].hunger = 100;
+            players[i].fatigue = 100;
+        }
+        players[0].name = "Jose";
+        players[1].name = "Juan";
+        players[2].name = "Pedro";
     }
 }
--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -1,8 +1,15 @@
-﻿
+﻿using UnityEngine;
+using System;
+public enum StatePlayer{
+    SICKNESS,
+    OPEN_WOUND,
+    NORMAL
+};
+
 [System.Serializable]
-public class Player{
+public class Player: MonoBehaviour{
     public string name;
-    public string status;
+    public StatePlayer status = StatePlayer.NORMAL;
     public bool sex;
     public int capacity;
     public int max_weight;
@@ -10,4 +17,127 @@ public class Player{
     public int hunger;
     public int salubrity;
     public int fatigue;
+    public bool gluttony;
+    public bool sloth;
+    public bool greed;
+    public bool lust;
+    public bool pride;
+    public bool envy;
+    public bool wrath;
+
+    public int ChooseWithProbability(int[] probs){
+        int rand;
+        rand = UnityEngine.Random.Range(1,101);
+        for(int i = 0; i < probs.Length;i++){
+            if(i == probs.Length-1){
+                return i;
+            }
+            if(rand<=probs[i]){
+                return i;
+            }
+            else{
+                probs[i+1]+=probs[i];
+                return 0;
+            }
+
+        }
+        return 0;
+    }
+
+    public bool CanEnterTheParty(Player otherPlayer){
+        int probabilityChange = 0;
+        //conditions
+        if(otherPlayer.sex != sex)probabilityChange-=5;
+        if(lust)probabilityChange-=20;
+        if(sanity>80)probabilityChange+=5;
+        if(sanity<20)probabilityChange-=5;
+        if(pride)probabilityChange-=20;
+        if(fatigue>60)probabilityChange+=5;
+        if(salubrity>70)probabilityChange+=5;
+        if(salubrity<30)probabilityChange-=5;
+        //calculate prob
+        int[] probs = new int[2];
+        probs[0] = 60+probabilityChange;
+        if(probs[0]>100)probs[0] = 100;
+        if(probs[0]<0)probs[0] = 0;
+        probs[1] = 100 - probs[0];
+        return(Convert.ToBoolean(ChooseWithProbability(probs)));
+    }
+    public bool HaveToLeaveTheParty(Player otherPlayer){
+        int probabilityChange = 0;
+        //conditions
+        if(otherPlayer.sex != sex)probabilityChange+=5;
+        if(lust)probabilityChange+=20;
+        if(sanity>80)probabilityChange-=10;
+        if(sanity<20)probabilityChange+=10;
+        if(pride)probabilityChange+=20;
+        if(fatigue>60)probabilityChange-=5;
+        if(salubrity>70)probabilityChange-=5;
+        if(salubrity<30)probabilityChange+=5;
+        //calculate prob
+        int[] probs = new int[2];
+        probs[0] = 50+probabilityChange;
+        if(probs[0]>100)probs[0] = 100;
+        if(probs[0]<0)probs[0] = 0;
+        probs[1] = 100 - probs[0];
+        return(Convert.ToBoolean(ChooseWithProbability(probs)));
+    }
+    public bool ChoosePath(){
+        int probabilityChange = 0;
+        //conditions
+        //calculate prob
+        int[] probs = new int[2];
+        probs[0] = 50+probabilityChange;
+        if(probs[0]>100)probs[0] = 100;
+        if(probs[0]<0)probs[0] = 0;
+        probs[1] = 100 - probs[0];
+        return(Convert.ToBoolean(ChooseWithProbability(probs)));
+    }
+    public int WhoEat(Player[] otherPlayers){
+        int[] probs = new int[otherPlayers.Length];
+        int total=0;
+        for(int i = 0; i < otherPlayers.Length; i++){
+            int relevancyValue = 0;
+            if(otherPlayers[i].hunger>30)relevancyValue+=1;
+            if(otherPlayers[i].hunger>60)relevancyValue+=2;
+            if(otherPlayers[i].hunger>90)relevancyValue+=3;
+            if(otherPlayers[i].name == name){
+                relevancyValue+=2;
+                if(gluttony)relevancyValue+=7;
+            }
+            if(otherPlayers[i].salubrity<35)relevancyValue+=2;
+            if(otherPlayers[i].fatigue<40)relevancyValue+=1;
+            probs[i] = relevancyValue;
+            total+=relevancyValue;
+        }
+        for(int i = 0; i < probs.Length; i++){
+            probs[i]= (probs[i]*100)/total;
+        }
+        return(ChooseWithProbability(probs));
+    }
+    public int WhoPlay(Player[] otherPlayers){
+        int[] probs= new int[otherPlayers.Length];
+        int total=0;
+        for(int i = 0; i < otherPlayers.Length; i++){
+            int relevancyValue = 0;
+            if(otherPlayers[i].sanity>30)relevancyValue+=1;
+            if(otherPlayers[i].sanity>60)relevancyValue+=2;
+            if(otherPlayers[i].sanity>90)relevancyValue+=3;
+            if(otherPlayers[i].name == name){
+                relevancyValue+=2;
+                if(envy)relevancyValue+=7;
+            }
+            if(otherPlayers[i].salubrity<35)relevancyValue+=1;
+            if(otherPlayers[i].fatigue<20)relevancyValue+=2;
+            probs[i] = relevancyValue;
+            total+=relevancyValue;
+        }
+        for(int i = 0; i < probs.Length; i++){
+            probs[i]= (probs[i]*100)/total;
+        }
+        return(ChooseWithProbability(probs));
+    }
+
+
+
 }
