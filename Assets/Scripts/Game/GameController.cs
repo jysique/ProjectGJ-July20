20943f4af7077c25aa82eadcb10f2ca9dc09f64a
Sedruol <destructor2104992013@gmail.com>
2020-07-11 17:51:34 -0500

    changues in break room

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -6,31 +6,27 @@ public class GameController : MonoBehaviour
 {
     private int max_players = 5;
     private int current_players = 3;
-    public Player[] players;
+    [SerializeField] private GameObject parentPlayer;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Vector3[] posPlayer;
+    public GameObject[] players;
     private Button[] array;
     [Header("Objects")]
-    [SerializeField]
-    private Button btnSetting;
+    [SerializeField] private Button btnSetting;
     [Header("Panels")]
-    [SerializeField]
-    private GameObject panelStats;
-    [SerializeField]
-    private GameObject panelSetName;
-        [SerializeField]
-    private GameObject panelSettings;
+    [SerializeField] private GameObject panelStats;
+    [SerializeField] private GameObject panelSetName;
+    [SerializeField] private GameObject panelSettings;
     [Header("Prefab Player Icon")]
-    [SerializeField]
-    private GameObject prefabPlayer;
-    
-    [SerializeField]
-    private Transform panelPlayers;
+    [SerializeField] private GameObject prefabPlayer;
+    [SerializeField] private Transform panelPlayers;
+    private bool sapitoHuebon;
     private void Awake() {
-        players  = new Player[current_players];
+        players  = new GameObject[current_players];
         array = new Button[current_players];
         InitPanels();
-        InitPlayers();
         InstantiatePlayers();
-
+        sapitoHuebon = false;
     }
     int current_index = 0;
     void Start()
@@ -50,33 +46,34 @@ public class GameController : MonoBehaviour
     }
 
     void ShowPlayerStats(int index){
-        panelStats.transform.GetChild(1).GetComponent<Slider>().value = players[index].sanity;
-        panelStats.transform.GetChild(2).GetComponent<Slider>().value = players[index].hunger;
-        panelStats.transform.GetChild(3).GetComponent<Slider>().value = players[index].salubrity;
-        panelStats.transform.GetChild(4).GetComponent<Slider>().value = players[index].fatigue;
-        string _sex = (players[index].sex)? " MUJER":"HOMBRE";
+        panelStats.transform.GetChild(1).GetComponent<Slider>().value = players[index].GetComponent<Player>().sanity;
+        panelStats.transform.GetChild(2).GetComponent<Slider>().value = players[index].GetComponent<Player>().hunger;
+        panelStats.transform.GetChild(3).GetComponent<Slider>().value = players[index].GetComponent<Player>().salubrity;
+        panelStats.transform.GetChild(4).GetComponent<Slider>().value = players[index].GetComponent<Player>().fatigue;
+        string _sex = (players[index].GetComponent<Player>().sex)? " MUJER":"HOMBRE";
         panelStats.transform.GetChild(5).GetComponent<Text>().text="SEX: "+ _sex;
         
         string _status = " ";
-        for (int i = 0; i < players[index].getStatus().Length; i++)
+        for (int i = 0; i < players[index].GetComponent<Player>().getStatus().Length; i++)
         {
-            _status+= players[index].getStatus()[i] + " ";
+            _status+= players[index].GetComponent<Player>().getStatus()[i] + " ";
         }
         panelStats.transform.GetChild(6).GetComponent<Text>().text="STATUS: "+ _status;
-        panelStats.transform.GetChild(7).GetComponent<Text>().text="CAPACITY:"+ players[index].capacity.ToString();
-        panelStats.transform.GetChild(8).GetComponent<Text>().text="WEIGHT MAX:"+ players[index].max_weight.ToString();
+        panelStats.transform.GetChild(7).GetComponent<Text>().text="CAPACITY:"+ players[index].GetComponent<Player>().capacity.ToString();
+        panelStats.transform.GetChild(8).GetComponent<Text>().text="WEIGHT MAX:"+ players[index].GetComponent<Player>().max_weight.ToString();
         panelStats.transform.GetChild(9).GetComponent<Text>().text= players[index].name;
     }
     void Update()
     {
-        ShowPlayerStats(current_index);
+        if (sapitoHuebon)
+            ShowPlayerStats(current_index);
     }
     //=============
     void AddStatus(){
-        players[current_index].addStatus("ENFERMO");
+        players[current_index].GetComponent<Player>().addStatus("ENFERMO");
     }
     void RemoveStatus(){
-        players[current_index].deleteStatus(1);
+        players[current_index].GetComponent<Player>().deleteStatus(1);
     }
     void InstantiatePlayers(){
         if (players.Length<= max_players)
@@ -92,12 +89,12 @@ public class GameController : MonoBehaviour
     void InitPlayers(){
         for (int i = 0; i < players.Length; i++)
         {
-            players[i] = new Player();   
-            players[i].sanity = 60;
-            players[i].salubrity = 100;
-            players[i].hunger = 100;
-            players[i].fatigue = 100;
-            players[i].status = new string[]{"N","ENF","XDD","AA"};
+            players[i] = Instantiate(player, posPlayer[i], Quaternion.identity, parentPlayer.transform);
+            players[i].GetComponent<Player>().sanity = 60;
+            players[i].GetComponent<Player>().salubrity = 100;
+            players[i].GetComponent<Player>().hunger = 100;
+            players[i].GetComponent<Player>().fatigue = 100;
+            players[i].GetComponent<Player>().status = new string[]{"N","ENF","XDD","AA"};
         }
         players[0].name = "Jose";
         players[1].name = "Juan";
@@ -115,13 +112,22 @@ public class GameController : MonoBehaviour
     void SetName(){
         panelSetName.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(()=>RegisterPlayer());
     }
-    void RegisterPlayer(){
+    void RegisterPlayer()
+    {
+        InitPlayers();
         InputField _temp =  panelSetName.transform.GetChild(2).GetComponent<InputField>();
-        players[0].setName(_temp.text);
+        Text temp = _temp.transform.GetChild(2).GetComponent<Text>();
         panelStats.SetActive(true);
         panelPlayers.gameObject.SetActive(true);
         panelSetName.SetActive(false);
         ButtonsFuncionality();
+        Debug.Log("del sapo huevon: " + _temp.text);
+        Debug.Log("del koala: " + temp.text);
+        players[0].GetComponent<Player>().setName(_temp.text);
+        panelPlayers.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = _temp.text;
+        sapitoHuebon = true;
+        players[0].name = _temp.text;
+        current_index = 0;
         // ShowPlayerStats(0);
     }
     void GoSettings(){
