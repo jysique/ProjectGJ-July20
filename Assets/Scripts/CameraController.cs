6ebe5a78474e9753a3e295f634a59a2cf82d1ce7
Sanca Venegas, Martin Joel <splayer003@gmail.com>
2020-07-11 22:11:51 -0500

    added cameraController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform cameraPos;
+    public float scrollSpeed;
+
+    public float topBarrier;
+    public float botBarrier;
+    public float leftBarrier;
+    public float rightBarrier;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.mousePosition.y >= Screen.height * topBarrier) {
+            cameraPos.Translate(Vector3.forward * Time.deltaTime * scrollSpeed, Space.World);
+        }
+
+        if (Input.mousePosition.y <= Screen.height * botBarrier) {
+            cameraPos.Translate(-Vector3.forward * Time.deltaTime * scrollSpeed, Space.World);
+        }
+
+        if (Input.mousePosition.x >= Screen.width * rightBarrier) {
+            cameraPos.Translate(Vector3.right * Time.deltaTime * scrollSpeed, Space.World);
+        }
+
+        if (Input.mousePosition.x <= Screen.height * leftBarrier) {
+            cameraPos.Translate(Vector3.left * Time.deltaTime * scrollSpeed, Space.World);
+        }
+    }
+}
