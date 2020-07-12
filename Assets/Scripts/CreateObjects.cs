067b7c3a14ecd03ed19e3b82f3ca2565ec4e7f31
Sedruol <destructor2104992013@gmail.com>
2020-07-11 21:24:10 -0500

    change in objects' creation

--- Assets/Scripts/CreateObjects.cs
+++ Assets/Scripts/CreateObjects.cs
@@ -20,30 +20,40 @@ public class CreateObjects : MonoBehaviour
     {
         for(int i = 0; i < cantObjects; i++)
         {
-            prefabSelected = Random.Range(0, 4);
+            prefabSelected = Random.Range(0, prefabsObjects.Count);
             newPosX = Random.Range(0, 2);
-            newPosZ = Random.Range(0, 2);
             if (prefabSelected == 0)
-                Instantiate(prefabsObjects[0], new Vector3(posX, 0, posZ), Quaternion.identity, this.transform);
+                Instantiate(prefabsObjects[0], new Vector3(posX, 0.5f, posZ), Quaternion.identity, this.transform);
             else if (prefabSelected == 1)
-                Instantiate(prefabsObjects[1], new Vector3(posX, 0, posZ), Quaternion.identity, this.transform);
+                Instantiate(prefabsObjects[1], new Vector3(posX, 0.5f, posZ), Quaternion.identity, this.transform);
             else if (prefabSelected == 2)
-                Instantiate(prefabsObjects[2], new Vector3(posX, 0, posZ), Quaternion.identity, this.transform);
+                Instantiate(prefabsObjects[2], new Vector3(posX, 0.5f, posZ), Quaternion.identity, this.transform);
             else if (prefabSelected == 3)
-                Instantiate(prefabsObjects[3], new Vector3(posX, 0, posZ), Quaternion.identity, this.transform);
+                Instantiate(prefabsObjects[3], new Vector3(posX, 0.5f, posZ), Quaternion.identity, this.transform);
+            else if (prefabSelected == 4)
+                Instantiate(prefabsObjects[4], new Vector3(posX, 0.5f, posZ), Quaternion.identity, this.transform);
+            else if (prefabSelected == 5)
+                Instantiate(prefabsObjects[5], new Vector3(posX, 0.5f, posZ), Quaternion.identity, this.transform);
+            else if (prefabSelected == 6)
+                Instantiate(prefabsObjects[6], new Vector3(posX, 0.5f, posZ), Quaternion.identity, this.transform);
+            else if (prefabSelected == 7)
+                Instantiate(prefabsObjects[7], new Vector3(posX, 0.5f, posZ), Quaternion.identity, this.transform);
+            else if (prefabSelected == 8)
+                Instantiate(prefabsObjects[8], new Vector3(posX, 0.5f, posZ), Quaternion.identity, this.transform);
+            else if (prefabSelected == 9)
+                Instantiate(prefabsObjects[9], new Vector3(posX, 0.5f, posZ), Quaternion.identity, this.transform);
             //
-            if (newPosX == 0 && posX + 2 > 8)
-                posX -= 2;
-            else if (newPosX == 0 && posX + 2 < 8)
-                posX += 2;
+            if (newPosX == 0 && posX + 3 > 4)
+                posX -= 3;
+            else if (newPosX == 0 && posX + 3 < 4)
+                posX += 3;
             //
-            if (newPosX == 1 && posX - 2 < -8)
-                posX += 2;
-            else if (newPosX == 1 && posX - 2 > -8)
-                posX -= 2;
+            if (newPosX == 1 && posX - 2 < -4)
+                posX += 3;
+            else if (newPosX == 1 && posX - 3 > -4)
+                posX -= 3;
             //
-            if (newPosZ == 0)
-                posZ += 2;
+            posZ += 5;
         }
     }
 
